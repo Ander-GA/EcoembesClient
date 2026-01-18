8202cae9b35c8ef0551038149ce0f5a3b4437001
Ander Gonzalez <ander.gonzalez.a@opendeusto.es>
2026-01-18 15:59:48 +0100

    cambios de dto y insercion del proxy

---
+++ EcoembesClient/src/main/java/es/deusto/sd/eoembes/client/proxies/IEcoembesServiceProxy.java
@@ -0,0 +1,18 @@
+package es.deusto.sd.eoembes.client.proxies;
+
+import java.util.List;
+import es.deusto.sd.ecoembes.client.dto.*;
+
+public interface IEcoembesServiceProxy {
+    // Autenticación
+    String login(CredentialsDTO credentials);
+    void logout(String token);
+
+    // Contenedores
+    ContainerDTO createContainer(ContainerDTO container, String token);
+    List<NivelLlenadoDTO> getHistorial(Long id, String fechaInicio, String fechaFin); // Fechas como String para simplificar
+    List<EstadoContenedorDTO> getContenedoresPorZona(int codigoPostal, String fecha);
+
+    // Plantas
+    void asignarContenedores(Long plantaId, AsignacionMasivaDTO asignacion);
+}
