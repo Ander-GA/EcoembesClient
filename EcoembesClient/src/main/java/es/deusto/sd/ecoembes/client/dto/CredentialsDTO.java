8c2bdab60f09a169f6a2dfe8307a1baff3a76472
Ander Gonzalez <ander.gonzalez.a@opendeusto.es>
2026-01-18 16:18:33 +0100

    cambio en dto

--- EcoembesClient/src/main/java/es/deusto/sd/ecoembes/client/dto/CredentialsDTO.java
+++ EcoembesClient/src/main/java/es/deusto/sd/ecoembes/client/dto/CredentialsDTO.java
@@ -1,23 +1,31 @@
 package es.deusto.sd.ecoembes.client.dto;
 
 public class CredentialsDTO {
-	private String email;
-	private String password;
 
-	// Getters y Setters
-	public String getEmail() {
-		return email;
-	}
+    private String email;
+    private String password;
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public CredentialsDTO() {
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public CredentialsDTO(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
